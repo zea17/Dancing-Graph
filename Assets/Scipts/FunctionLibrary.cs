983592cd741e1ff6fd7dc0f55d8ad523767458cc
zea <zeaak17@gmail.com>
2025-10-05 16:32:15 +0330

    add transition between functions

--- Assets/Scipts/FunctionLibrary.cs
+++ Assets/Scipts/FunctionLibrary.cs
@@ -3,28 +3,29 @@ using static UnityEngine.Mathf;
 
 public static class FunctionLibrary
 {
-    public delegate Vector3 Function(float u, float v, float t);
+	public delegate Vector3 Function(float u, float v, float t);
 
-    public enum FunctionName { Wave, MultiWave, Ripple, Sphere, Torus }
+	public enum FunctionName { Wave, MultiWave, Ripple, Sphere, Torus }
 
-    static Function[] functions = { Wave, MultiWave, Ripple, Sphere, Torus };
+	static Function[] functions = { Wave, MultiWave, Ripple, Sphere, Torus };
 
-    public static Function GetFunction(FunctionName name)
-    {
-        return functions[(int)name];  // Explicitly cast enum to int
-    }
+	public static Function GetFunction(FunctionName name)
+	{
+		return functions[(int)name];  // Explicitly cast enum to int
+	}
 
-    public static Vector3 Wave (float u, float v, float t){
-        Vector3 p;
+	public static Vector3 Wave(float u, float v, float t)
+	{
+		Vector3 p;
 		p.x = u;
 		p.y = Sin(PI * (u + v + t));
 		p.z = v;
 		return p;
-    }
+	}
 
-    public static Vector3 MultiWave(float u, float v, float t)
-    {
-	Vector3 p;
+	public static Vector3 MultiWave(float u, float v, float t)
+	{
+		Vector3 p;
 		p.x = u;
 		p.y = Sin(PI * (u + 0.5f * t));
 		p.y += 0.5f * Sin(2f * PI * (v + t));
@@ -32,9 +33,10 @@ public static class FunctionLibrary
 		p.y *= 1f / 2.5f;
 		p.z = v;
 		return p;
-    }
+	}
 
- public static Vector3 Ripple (float u, float v, float t) {
+	public static Vector3 Ripple(float u, float v, float t)
+	{
 		float d = Sqrt(u * u + v * v);
 		Vector3 p;
 		p.x = u;
@@ -44,7 +46,8 @@ public static class FunctionLibrary
 		return p;
 	}
 
-    public static Vector3 Sphere (float u, float v, float t) {
+	public static Vector3 Sphere(float u, float v, float t)
+	{
 		float r = 0.9f + 0.1f * Sin(PI * (6f * u + 4f * v + t));
 		float s = r * Cos(0.5f * PI * v);
 		Vector3 p;
@@ -54,8 +57,9 @@ public static class FunctionLibrary
 		return p;
 	}
 
-    public static Vector3 Torus (float u, float v, float t) {
-        float r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
+	public static Vector3 Torus(float u, float v, float t)
+	{
+		float r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
 		float r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * t));
 		float s = r1 + r2 * Cos(PI * v);
 		Vector3 p;
@@ -63,5 +67,23 @@ public static class FunctionLibrary
 		p.y = r2 * Sin(PI * v);
 		p.z = s * Cos(PI * u);
 		return p;
+	}
+	public static FunctionName GetNextFunctionName(FunctionName name)
+	{
+		return (int)name < functions.Length - 1 ? name + 1 : 0;
+	}
+
+	public static FunctionName GetRandomFunctionNameOtherThan(FunctionName name)
+	{
+		var choice = (FunctionName)Random.Range(1, functions.Length);
+		return choice == name ? 0 : choice;
+	}
+	public static Vector3 Morph(
+			float u, float v, float t, Function from, Function to, float progress
+		)
+	{
+		return Vector3.LerpUnclamped(
+		from(u, v, t), to(u, v, t), SmoothStep(0f, 1f, progress)
+	);
 	}
 }
--- Assets/Scipts/Graph.cs
+++ Assets/Scipts/Graph.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 
+
+
 public class Graph : MonoBehaviour
 {
     [SerializeField]
@@ -11,15 +13,28 @@ public class Graph : MonoBehaviour
     [SerializeField]
     FunctionLibrary.FunctionName function;
 
+    public enum TransitionMode { Cycle, Random }
+
+    [SerializeField]
+    TransitionMode transitionMode;
+
+    [SerializeField, Min(0f)]
+    float functionDuration = 1f, transitionDuration = 1f;
+
     Transform[] points;
 
+    float duration;
+    bool transitioning;
+
+    FunctionLibrary.FunctionName transitionFunction;
     void Awake()
     {
         float step = 2f / resolution;
         //var position = Vector3.zero;
         var scale = Vector3.one * step;
         points = new Transform[resolution * resolution];
-        for (int i = 0; i < points.Length; i++){
+        for (int i = 0; i < points.Length; i++)
+        {
             Transform point = points[i] = Instantiate(pointPrefab);
 
             point.localScale = scale;
@@ -29,20 +44,82 @@ public class Graph : MonoBehaviour
 
     void Update()
     {
+        duration += Time.deltaTime;
+        if (transitioning)
+        {
+            if (duration >= transitionDuration)
+            {
+                duration -= transitionDuration;
+                transitioning = false;
+            }
+        }
+        else if (duration >= functionDuration)
+        {
+            duration -= functionDuration;
+            transitioning = true;
+            transitionFunction = function;
+            PickNextFunction();
+        }
+
+        if (transitioning)
+        {
+            UpdateFunctionTransition();
+        }
+        else
+        {
+            UpdateFunction();
+        }
+    }
+    void PickNextFunction()
+    {
+        function = transitionMode == TransitionMode.Cycle ?
+            FunctionLibrary.GetNextFunctionName(function) :
+            FunctionLibrary.GetRandomFunctionNameOtherThan(function);
+    }
+    void UpdateFunction()
+    {
         FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
         float time = Time.time;
         float step = 2f / resolution;
         float v = 0.5f * step - 1f;
         for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
         {
-         if (x == resolution) {
-				x = 0;
-				z += 1;
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
                 v = (z + 0.5f) * step - 1f;
-			}
-			float u = (x + 0.5f) * step - 1f;
-			//float v = (z + 0.5f) * step - 1f;
-			points[i].localPosition = f(u, v, time);
+            }
+            float u = (x + 0.5f) * step - 1f;
+            //float v = (z + 0.5f) * step - 1f;
+            points[i].localPosition = f(u, v, time);
         }
     }
+
+    void UpdateFunctionTransition()
+    {
+        FunctionLibrary.Function
+            from = FunctionLibrary.GetFunction(transitionFunction),
+            to = FunctionLibrary.GetFunction(function);
+        float progress = duration / transitionDuration;
+        float time = Time.time;
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
+        {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f;
+            }
+            float u = (x + 0.5f) * step - 1f;
+            //float v = (z + 0.5f) * step - 1f;
+            points[i].localPosition = FunctionLibrary.Morph(
+                 u, v, time, from, to, progress
+             );
+        }
+    }
+
+
 }
