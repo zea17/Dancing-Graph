1e4775873db3c1a2f23263ecc51581c59caf2189
zea <zeaak17@gmail.com>
2025-10-05 15:52:15 +0330

    Enhance FrameRateCounter to track and display best and worst frame durations

--- Assets/Scipts/FrameRateCounter.cs
+++ Assets/Scipts/FrameRateCounter.cs
@@ -7,10 +7,15 @@ public class FrameRateCounter : MonoBehaviour
     [SerializeField]
     TextMeshProUGUI display;
 
+    public enum DisplayMode { FPS, MS }
+
+    [SerializeField]
+    DisplayMode displayMode = DisplayMode.FPS;
+
     [SerializeField, Range(0.1f, 2f)]
     float sampleDuration = 1f;
     int frames;
-    float duration;
+    float duration, bestDuration = float.MaxValue, worstDuration;
 
     void Update()
     {
@@ -18,11 +23,38 @@ public class FrameRateCounter : MonoBehaviour
         frames += 1;
         duration += frameDuration;
 
+        if (frameDuration < bestDuration)
+        {
+            bestDuration = frameDuration;
+        }
+        if (frameDuration > worstDuration)
+        {
+            worstDuration = frameDuration;
+        }
         if (duration >= sampleDuration)
         {
-            display.SetText("FPS\n{0:0}\n000\n000", frames / duration);
+            if (displayMode == DisplayMode.FPS)
+            {
+                display.SetText(
+                    "FPS\n{0:0}\n{1:0}\n{2:0}",
+                    1f / bestDuration,
+                    frames / duration,
+                    1f / worstDuration
+                );
+            }
+            else
+            {
+                display.SetText(
+                    "MS\n{0:1}\n{1:1}\n{2:1}",
+                    1000f * bestDuration,
+                    1000f * duration / frames,
+                    1000f * worstDuration
+                );
+            }
             frames = 0;
             duration = 0f;
+            bestDuration = float.MaxValue;
+            worstDuration = 0f;
         }
     }
 }
