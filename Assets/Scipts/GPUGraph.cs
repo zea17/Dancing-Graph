3749055e32f2ac538bad6a3e088c4adccf5c01c4
zea <zeaak17@gmail.com>
2025-11-19 13:45:19 +0330

    Add GPUGraph class for GPU function transitions

---
+++ Assets/Scipts/GPUGraph.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class GPUGraph : MonoBehaviour
+{
+
+    [SerializeField, Range(10, 200)]
+    int resolution = 10;
+
+    [SerializeField]
+    FunctionLibrary.FunctionName function;
+
+    public enum TransitionMode { Cycle, Random }
+
+    [SerializeField]
+    TransitionMode transitionMode = TransitionMode.Cycle;
+
+    [SerializeField, Min(0f)]
+    float functionDuration = 1f, transitionDuration = 1f;
+
+
+    float duration;
+    bool transitioning;
+
+    FunctionLibrary.FunctionName transitionFunction;
+
+
+    void Update()
+    {
+        duration += Time.deltaTime;
+        if (transitioning)
+        {
+            if (duration >= transitionDuration)
+            {
+                duration -= transitionDuration;
+                transitioning = false;
+            }
+        }
+        else if (duration >= functionDuration)
+        {
+            duration -= functionDuration;
+            transitioning = true;
+            transitionFunction = function;
+            PickNextFunction();
+        }
+
+    }
+    void PickNextFunction()
+    {
+        function = transitionMode == TransitionMode.Cycle ?
+            FunctionLibrary.GetNextFunctionName(function) :
+            FunctionLibrary.GetRandomFunctionNameOtherThan(function);
+    }
+
+
+}
