cbc9f9d6af3610718ce5fc11e4a03ab0eee7bc33
zea <zeaak17@gmail.com>
2025-11-27 14:13:56 +0330

    Refactor GPUGraph to initialize material and mesh, and update GPU function call in Update method

--- Assets/Scipts/GPUGraph.cs
+++ Assets/Scipts/GPUGraph.cs
@@ -1,9 +1,15 @@
+using System;
 using UnityEngine;
 
 public class GPUGraph : MonoBehaviour
 {
     [SerializeField]
-	ComputeShader computeShader;
+    Material material;
+
+    [SerializeField]
+    Mesh mesh;
+    [SerializeField]
+    ComputeShader computeShader;
 
     [SerializeField, Range(10, 200)]
     int resolution = 10;
@@ -26,7 +32,23 @@ public class GPUGraph : MonoBehaviour
     FunctionLibrary.FunctionName transitionFunction;
 
     ComputeBuffer positionsBuffer;
-
+    static readonly int
+     positionsId = Shader.PropertyToID("_Positions"),
+     resolutionId = Shader.PropertyToID("_Resolution"),
+     stepId = Shader.PropertyToID("_Step"),
+     timeId = Shader.PropertyToID("_Time");
+    void UpdateFunctionOnGPU()
+    {
+        float step = 2f / resolution;
+        computeShader.SetInt(resolutionId, resolution);
+        computeShader.SetFloat(stepId, step);
+        computeShader.SetFloat(timeId, Time.time);
+        computeShader.SetBuffer(0, positionsId, positionsBuffer);
+        int groups = Mathf.CeilToInt(resolution / 8f);
+        computeShader.Dispatch(0, groups, groups, 1);
+        var bounds = new Bounds(Vector3.zero, Vector3.one * (2f + 2f / resolution));
+        Graphics.DrawMeshInstancedProcedural(mesh, 0, material, bounds, positionsBuffer.count);
+    }
     void OnEnable()
     {
         positionsBuffer = new ComputeBuffer(resolution * resolution, 3 * 4);
@@ -54,6 +76,7 @@ public class GPUGraph : MonoBehaviour
             transitionFunction = function;
             PickNextFunction();
         }
+        UpdateFunctionOnGPU();
 
     }
     void PickNextFunction()
